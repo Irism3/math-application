b682c6fc7c2469ea0692d50b12b2ec097e03efc1
irismelendez <iris.13melendez@gmail.com>
2025-09-25 00:01:47 -0500

    i made changes to MathApp

--- src/main/java/pluralsight/MathApp.java
+++ src/main/java/pluralsight/MathApp.java
@@ -1,4 +1,23 @@
 package pluralsight;
 
 public class MathApp {
+    public static void main(String[] args) {
+        // Question 1.
+        //1.creating a salary
+        double bobSalary = 60.000;
+        double garySalary = 90.000;
+
+         // 2 the highest salary
+        double highestSalary =Math.max(bobSalary, garySalary);
+        System.out.println("the highest salary is $" + highestSalary);
+
+        //question 2.
+        double carPrice = 70.000;
+        double truckPrice= 40.000;
+        System.out.println("the smallest price is $ "+ Math.min(carPrice, truckPrice));
+
+        //question 3
+
+
+    }
 }
